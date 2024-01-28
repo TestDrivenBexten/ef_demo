c6bf39553e2c68a1e766254dfdba6363f4776efd
Skyler Bexten <skyler.bexten@gmail.com>
2024-01-28 15:23:54 -0800

    Write entries to database

--- Program.cs
+++ Program.cs
@@ -6,10 +6,13 @@ using var db = new DemoContext();
 var account = new Account { AccountNumber = "X1000", OwnerName="Jack Daniels" };
 var share = new Share { ShareAmount = 10.00M };
 account.Shares.Add(share);
+db.Accounts.Add(account);
 db.SaveChanges();
 
 Console.WriteLine("Account saved");
 
-var fetchedAccount = db.Accounts.First();
-Console.WriteLine(fetchedAccount);
+var fetchedAccount = db.Accounts
+    .First(x => x.AccountId == account.AccountId);
+Console.WriteLine($"Fetched account: {fetchedAccount.AccountNumber}");
+Console.WriteLine($"Share count: {fetchedAccount.Shares.Count}");
 
